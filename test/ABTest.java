e93c85800ea7f1db2a00460f6683a74d8a3d8194
tsalakhe <Timur.Salakhetdinov@t-systems.ru>
2017-06-09 09:21:37 +0300

    Initial commit

---
+++ test/ABTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Created by tsalakhe on 09.06.2017.
+ */
+public class ABTest {
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}

284e86943f15eb6603c4bc8986c5dcbb492eb264
Timur <timur.salakhetdinov@gmail.com>
2017-06-11 02:35:15 +0300

    add first test for ANewHope problem

--- src/topcoder/ANewHope.java
+++ src/topcoder/ANewHope.java
@@ -4,4 +4,7 @@ package topcoder;
  * Created by Timur on 11-Jun-17.
  */
 public class ANewHope {
+    public int count(int[] firstWeek, int[] lastWeek, int daysToWashAShirt) {
+        return 1;
+    }
 }
--- test/topcoder/ANewHopeTest.java
+++ test/topcoder/ANewHopeTest.java
@@ -1,15 +1,24 @@
 package topcoder;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Created by Timur on 11-Jun-17.
  */
 public class ANewHopeTest {
+
+    private ANewHope aNewHope;
+
+    @BeforeEach
+    void setUp() {
+        aNewHope = new ANewHope();
+    }
+
     @Test
-    void test() {
-        new ANewHope();
+    void count() {
+        assertEquals(1, aNewHope.count(new int[]{1, 2}, new int[]{1, 2}, 1));
     }
 }
